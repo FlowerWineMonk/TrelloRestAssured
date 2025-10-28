03e7b3662e15111757f57bd264da5fd8f763f812
FlowerWineMonk <hidekitakuya0@gmail.com>
2025-10-28 12:41:28 +0500

    feat: update secrets handling

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,6 +1,7 @@
 package base;
 
-import configLoader.ConfigLoader;
+import utils.ConfigReader;
+import utils.EnvReader;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
@@ -9,25 +10,25 @@ import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
 public class BaseTest {
-        private static final RequestSpecification reqSpec = new RequestSpecBuilder()
-                .setBaseUri(ConfigLoader.get("BASE_URI"))
-                .setContentType(ContentType.JSON)
-                .setAccept(ContentType.JSON)
-                .addQueryParam("key", ConfigLoader.get("API_KEY"))
-                .addQueryParam("token", ConfigLoader.get("TOKEN"))
-                .addFilter(new AllureRestAssured())
-                .build();
+  private static final RequestSpecification reqSpec = new RequestSpecBuilder()
+      .setBaseUri(ConfigReader.get("BASE_URI"))
+      .setContentType(ContentType.JSON)
+      .setAccept(ContentType.JSON)
+      .addQueryParam("key", EnvReader.get("API_KEY"))
+      .addQueryParam("token", EnvReader.get("TOKEN"))
+      .addFilter(new AllureRestAssured())
+      .build();
 
-        private static final ResponseSpecification resSpec = new ResponseSpecBuilder()
-                .expectStatusCode(200)
-                .expectContentType(ContentType.JSON)
-                .build();
+  private static final ResponseSpecification resSpec = new ResponseSpecBuilder()
+      .expectStatusCode(200)
+      .expectContentType(ContentType.JSON)
+      .build();
 
-        protected static RequestSpecification getReqSpec() {
-            return reqSpec;
-        }
+  protected static RequestSpecification getReqSpec() {
+    return reqSpec;
+  }
 
-        protected static ResponseSpecification getResSpec() {
-            return resSpec;
-        }
+  protected static ResponseSpecification getResSpec() {
+    return resSpec;
+  }
 }
