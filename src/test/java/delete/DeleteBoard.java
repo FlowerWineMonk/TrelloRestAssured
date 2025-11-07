ae3f2c0359782276bc45c96284ddb3afe109f6fb
FlowerWineMonk <hidekitakuya0@gmail.com>
2025-11-07 14:57:28 +0500

    feat: add deleteAllBoards to prevent reaching the limit of board creation

--- src/test/java/delete/DeleteBoard.java
+++ src/test/java/delete/DeleteBoard.java
@@ -4,6 +4,8 @@ import base.BaseTest;
 import enums.BoardEndpoints;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.nullValue;
 
@@ -27,5 +29,36 @@ public class DeleteBoard extends BaseTest {
                 .then()
                 .statusCode(200)
                 .body("id", nullValue());
+    }
+
+    @Test
+    public void deleteAllBoards() {
+        List<String> boardIds =
+                given()
+                        .spec(getDefaultRequestSpecification())
+                        .when()
+                        .get("/members/me/boards")
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .path("id");
+
+        System.out.println("Found " + boardIds.size() + " boards. Deleting...");
+
+        if (boardIds == null || boardIds.isEmpty()) {
+            System.out.println("There are no boards. Batch deletion will be skipped!");
+        } else {
+            for (String boardId : boardIds) {
+                given()
+                        .spec(getDefaultRequestSpecification())
+                        .when()
+                        .delete(BoardEndpoints.DELETE_BOARD.getEndpoint(), boardId)
+                        .then()
+                        .statusCode(200);
+
+                System.out.println("Deleted: " + boardId);
+            }
+            System.out.println("✅ All boards deleted.");
+        }
     }
 }
